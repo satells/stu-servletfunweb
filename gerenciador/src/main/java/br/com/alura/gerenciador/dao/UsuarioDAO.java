0d786bbe59dbc14860feb4a0189d40559b046e48
mauro <mauro@mauro-PC>
2021-01-13 10:03:02 -0300

    alterações gerais

--- gerenciador/src/main/java/br/com/alura/gerenciador/dao/UsuarioDAO.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/dao/UsuarioDAO.java
@@ -7,21 +7,22 @@ import br.com.alura.gerenciador.Usuario;
 
 public class UsuarioDAO {
 
-	private final static Map<String, Usuario> USUARIOS = new HashMap<>();
-	static {
-		USUARIOS.put("guilherme.silveira@alura.com.br", new Usuario("guilherme.silveira@alura.com.br","silveira"));
-		USUARIOS.put("rodrigo.turini@alura.com.br", new Usuario("rodrigo.turini@alura.com.br","turini"));
-	}
+    private final static Map<String, Usuario> USUARIOS = new HashMap<>();
+    static {
+	USUARIOS.put("guilherme.silveira@alura.com.br", new Usuario("guilherme.silveira@alura.com.br", "silveira"));
+	USUARIOS.put("rodrigo.turini@alura.com.br", new Usuario("rodrigo.turini@alura.com.br", "turini"));
+	USUARIOS.put("msergiost@hotmail.com", new Usuario("mauro@hotmail.com", "123"));
+    }
 
-	public Usuario buscaPorEmailESenha(String email, String senha) {
-		if (!USUARIOS.containsKey(email))
-			return null;
+    public Usuario buscaPorEmailESenha(String email, String senha) {
+	if (!USUARIOS.containsKey(email))
+	    return null;
 
-		Usuario usuario = USUARIOS.get(email);
-		if (usuario.getSenha().equals(senha))
-			return usuario;
-		
-		return null;
-	}
+	Usuario usuario = USUARIOS.get(email);
+	if (usuario.getSenha().equals(senha))
+	    return usuario;
+
+	return null;
+    }
 
 }
---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/filter/FiltroDeAuditoria.java
@@ -0,0 +1,52 @@
+package br.com.alura.gerenciador.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class FiltroDeAuditoria implements Filter {
+
+    public FiltroDeAuditoria() {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	HttpServletRequest req = (HttpServletRequest) request;
+	String uri = req.getRequestURI();
+
+	String usuario = getUsuario(req);
+
+	System.out.println("Usuário " + usuario + " acessando a URI: " + uri);
+	chain.doFilter(request, response);
+
+    }
+
+    private String getUsuario(HttpServletRequest req) {
+	Cookie[] cookies = req.getCookies();
+	String usuario = "<deslogado>";
+	if (cookies == null)
+	    return usuario;
+
+	for (Cookie cookie : cookies) {
+	    if (cookie.getName().equals("usuario.logado")) {
+		usuario = cookie.getValue();
+	    }
+	}
+	return usuario;
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+}
